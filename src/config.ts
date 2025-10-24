bbd3951584ba9831ad6a8fac2960f33fff451288
sigvaldr <sigvaldrnotthrafn@proton.me>
2025-10-23 21:59:05 -0400

    restructured apply page

--- src/config.ts
+++ src/config.ts
@@ -20,4 +20,6 @@ export const SITE = {
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  discordLink: "https://discord.gg/B3zTeyK97e",
+  applicationLink: "http://yggdrasil.vikingtactical.us:9999/apps/forms/s/8fqDa8zmjgowxqC8tgLdxsdk",
 } as const;
